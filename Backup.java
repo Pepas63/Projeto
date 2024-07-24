1c14a54089a45f9e8982d4f52d18b8b2badd1e7a
1Seirei1 <142747827+1Seirei1@users.noreply.github.com>
2024-07-24 23:08:10 +0100

    Update Backup.java

--- Backup.java
+++ Backup.java
@@ -4,13 +4,13 @@ import java.io.*;
 import java.util.ArrayList;
 
 public class Backup {
-		public static void gravaClientes(ArrayList<Cliente> clientes, String fichaclientes) {
-		File f = new File(fichaclientes);
+		public static void salvarClientes(ArrayList<Cliente> clientes, String listaclientes) {
+		File f = new File(listaclientes);
 		try{
 			f.createNewFile();				// Criar um novo ficheiro
-			ObjectOutputStream ficheiro = new ObjectOutputStream(new FileOutputStream(fichaclientes));
+			ObjectOutputStream ficheiro = new ObjectOutputStream(new FileOutputStream(listaclientes));
 			ficheiro.writeObject(clientes); // Escrever o arraylist no ficheiro
-			System.out.println("Backup de clientes executado com sucesso.");
+			System.out.println(" Lista de clientes  foi arquivada com sucesso.");
 			System.out.println("no ficheiro: " + f.getAbsolutePath());
 			ficheiro.flush();
 			ficheiro.close();
@@ -20,16 +20,16 @@ public class Backup {
 		}
 }
 @SuppressWarnings("unchecked")
-public static ArrayList<Cliente> lerClientes(String fichaclientes)
+public static ArrayList<Cliente> listarClientes(String listaclientes)
 	{
 	ArrayList<Cliente> clientes = new ArrayList <>();
-	File ficheiro = new File(fichaclientes);
+	File ficheiro = new File(listaclientes);
 	if (!ficheiro.exists())
-		ficheiro = new File(fichaclientes);
+		ficheiro = new File(listaclientes);
 	else {
 		ObjectInputStream f;
 		try {
-			f = new ObjectInputStream(new FileInputStream(fichaclientes));
+			f = new ObjectInputStream(new FileInputStream(listaclientes));
 			clientes = (ArrayList<Cliente>) f.readObject();
 			System.out.println("Restauro dos dados realizado com sucesso.");
 		}catch (IOException e) {
@@ -45,3 +45,4 @@ public static ArrayList<Cliente> lerClientes(String fichaclientes)
 }
 
 }
+    
